50c87013e971270cd850b38e81cada005ed31102
AlexisLanuza <LANUZAAC@mackillop.act.edu.au>
2016-05-30 21:19:37 +1000

    Input change
    altered input to oninput and altered variable declaration.

--- purpleringincome.js
+++ purpleringincome.js
@@ -1,10 +1,10 @@
 /* DOCTYPE Javascipt */
 
 //variables
-var incomeField = document.getElementById("incomeField");
-var difference[];
-var product [];
-var sum= [];
+var incomeField= document.getElementById("incomeField");
+var difference;
+var product;
+var sum;
 //constants below listed as var due to some browsers not supporting “const”
 //constants are in capital letters as per normal convention
 //income brackets
@@ -21,49 +21,55 @@ var TAXPER4 = 0.36;
 var ADDTAX1 = 3650;
 var ADDTAX2 = 13330;
 var ADDTAX3 = 30490;
+//Statement Display variable
+var statementDisplay.innerHTML("Tax");
+var displayZero = "No tax";
+var displayProduct = " Your Tax is " + product ;
+var displaySum = " Your Tax is" + sum ;
 
 
 //income tax calculator
-function calculateTax(income,BRAC1,BRAC2,BRAC3,BRAC4, difference, taxPer, product,addTax, sum ) {
 
- if (income < BRAC1) {
-  return 0;
+    function calculateTax() {
+        
     
-} else if (income < BRAC2) {;
+ if (incomeField < BRAC1) {
+  return 0;
+                                 statementDisplay.innerHTML= displayZero;
+    
+} else if (incomeField < BRAC2) {;
    difference = income - BRAC1;
    product = difference * TAXPER1 ;
     return product;
-
-  
-} else if (income < BRAC3) {; 
+                                 statementDisplay.innerHTML= displayProduct;
+                                
+} else if (incomeField < BRAC3) {; 
   difference = income - BRAC2;
   product = difference * TAXPER2;
   sum = product + ADDTAX1;
   return sum;
+                                 statementDisplay.innerhtml= displaySum;
  
-} else if (income < BRAC4) {
+} else if (incomeField < BRAC4) {
   difference = income - BRAC3;
   product = difference * TAXPER3;
   sum = product + ADDTAX2;
   return sum;
+                                 statementDisplay.innerhtml= displaySum;
+       
  
 } else { 
-  difference = income - BRAC4;
+  difference = incomeField - BRAC4;
   product = difference * TAXPER4;
   sum = product + ADDTAX3;
   return sum;
-  ))
-}
-    
-function alert(result){
-    if (return=0) {
-        alert("No Tax Due")
-    } else if (return=product) {
-        alert (" Your Tax is " + product)
-    } else {
-        alert ("Your Tax is sum");
-    }
-}
+                                 statementDisplay.innerHTML = displaySum;
+  
+}}
+
+
+
+
 
   
   
